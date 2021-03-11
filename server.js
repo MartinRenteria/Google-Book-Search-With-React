957342bdd4b0609882c83db631023901e2f77a91
Martin Renteria <renteriamp96@gmail.com>
2021-03-10 20:55:41 -0800

    Set-up express server

---
+++ server.js
@@ -0,0 +1,19 @@
+  
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
