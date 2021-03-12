0f866e82a1c2bc57278f018aeef7651d5fa4a4ed
Martin Renteria <renteriamp96@gmail.com>
2021-03-12 14:10:55 -0800

    installed needed dependencies and added onclick function to search button

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -3,14 +3,14 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './style.css'
 
-function Search() {
+function Search(props) {
     return (
 <Form>
   <Form.Group controlId="formBasicSearch">
     <Form.Label className="Search-text">Search for and save Books of Interest</Form.Label>
-    <Form.Control type="email" placeholder="Search for a book" />
+    <Form.Control type="text" placeholder="Search for a book" />
   </Form.Group>
-  <Button variant="primary" type="submit">
+  <Button onclick={props.handleFormSubmit} variant="primary" type="submit">
     Search
   </Button>
 </Form>
